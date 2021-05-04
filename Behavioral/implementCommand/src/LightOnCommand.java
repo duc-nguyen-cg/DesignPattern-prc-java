23aca657b153560b1f3e92ad912d1957f547886e
unknown <ngocduc.2109@gmail.com>
2021-05-04 15:04:27 +0700

    [Thực hành] Triển khai Command

---
+++ Behavioral/implementCommand/src/LightOnCommand.java
@@ -0,0 +1,12 @@
+public class LightOnCommand implements Command{
+    //reference to the light
+    Light light;
+
+    public LightOnCommand(Light light) {
+        this.light = light;
+    }
+
+    public void execute() {
+        light.switchOn();
+    }
+}
